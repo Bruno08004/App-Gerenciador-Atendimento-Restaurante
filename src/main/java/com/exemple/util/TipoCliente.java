abea337d03f9974860998c3c6d9aa0b20cd55178
Gabs-jg <gabriel19joao@gmail.com>
2025-06-04 22:54:46 -0300

    Documentação e exceção

--- src/main/java/com/exemple/util/TipoCliente.java
+++ src/main/java/com/exemple/util/TipoCliente.java
@@ -1,5 +1,26 @@
 package com.exemple.util;
 
+/**
+ * Enumeração que representa os tipos de clientes do restaurante.
+ * <p>
+ * Pode ser utilizada para diferenciar o atendimento de clientes prioritários e comuns.
+ * </p>
+ *
+ * <p><b>Exceções possíveis:</b></p>
+ * <ul>
+ *   <li>Não lança exceções diretamente, mas métodos que utilizam esta enumeração podem lançar {@link NullPointerException}
+ *   se um valor nulo for passado onde um {@code TipoCliente} é esperado.</li>
+ * </ul>
+ *
+ * <p><b>Uso típico:</b></p>
+ * <ol>
+ *   <li>Definir o tipo de um cliente ao instanciá-lo.</li>
+ *   <li>Utilizar o tipo para lógica de prioridade em filas ou atendimentos.</li>
+ * </ol>
+ *
+ * @author Seu Nome
+ * @version 1.0
+ */
 public enum TipoCliente {
     PRIORITARIO, COMUM
 }
--- src/main/java/com/exemple/util/Turno.java
+++ src/main/java/com/exemple/util/Turno.java
@@ -1,5 +1,26 @@
 package com.exemple.util;
 
+/**
+ * Enumeração que representa os turnos de funcionamento do restaurante.
+ * <p>
+ * Pode ser utilizada para controlar o turno de garçons, horários de atendimento e lógica de funcionamento.
+ * </p>
+ *
+ * <p><b>Exceções possíveis:</b></p>
+ * <ul>
+ *   <li>Não lança exceções diretamente, mas métodos que utilizam esta enumeração podem lançar {@link NullPointerException}
+ *   se um valor nulo for passado onde um {@code Turno} é esperado.</li>
+ * </ul>
+ *
+ * <p><b>Uso típico:</b></p>
+ * <ol>
+ *   <li>Definir o turno de um garçom ou do restaurante.</li>
+ *   <li>Utilizar o turno para lógica de controle de horários e permissões.</li>
+ * </ol>
+ *
+ * @author Seu Nome
+ * @version 1.0
+ */
 public enum Turno {
     MANHA, TARDE, NOITE
 }
